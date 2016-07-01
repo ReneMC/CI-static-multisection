cc45ea0c8e5ba57792d6de77c2afa472e0101ba6
ReneMC <rene.mejiac@gmail.com>
2016-07-01 15:42:25 -0700

    + Form validation fix

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -16,6 +16,11 @@
 <!-- Bootstrap Core JavaScript -->
 <script src="<?php echo base_url('/assets/js/bootstrap.min.js');?>"></script>
 
+<!-- Contact Form JavaScript -->
+<!-- Do not edit these files! In order to set the email address and subject line for the contact form go to the bin/contact_me.php file. -->
+<script src="<?php echo base_url('/assets/js/jqBootstrapValidation.js'); ?>"></script>
+<script src="<?php echo base_url('/assets/js/contact_me.js'); ?>"></script>
+
 <!-- Script to Activate the Carousel -->
 <script>
     $('.carousel').carousel({
